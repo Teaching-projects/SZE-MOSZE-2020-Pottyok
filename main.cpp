c3ea1c25e81b47968c360b95a6b7195ca2fc9e1c
mushukcha <penzespeter4@gmail.com>
2020-09-14 11:16:10 +0200

    Add argument check

--- main.cpp
+++ main.cpp
@@ -5,10 +5,24 @@
 
 using namespace std;
 
+int isdigits(const string & s) {
+    for (char c : s) if (!isdigit(c)) return (0);
+    return (1);
+}
+
 int main (int argc, char *argv[]) {
 
-    Entity* ent1 = new Entity(150,10,"Maple");
-    Entity* ent2 = new Entity(45,30,"Sally");
+    Entity* ent1;
+    Entity* ent2;
+
+    if(argc != 7 || !(isdigits(argv[2]) && isdigits(argv[3]) && isdigits(argv[5]) && isdigits(argv[6])) ){
+        std::cout << "Wrong initial data given, using default values." << std::endl;
+        ent1 = new Entity(150, 10, "Maple");
+        ent2 = new Entity(45, 30, "Sally");
+    }else{
+        ent1 = new Entity(stof(argv[2]), stof(argv[3]), argv[1]);
+        ent2 = new Entity(stof(argv[5]), stof(argv[6]), argv[4]);
+    }
 
     Game* game = new Game(*ent1,*ent2);
     game->Start();
