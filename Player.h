13d7ba6d796cd407438a6f864e5c4b7ece1ccde5
tamasori <oeri.tamas@gmail.com>
2020-10-10 13:16:22 +0200

    add loose gained levels and reset stats to start of fight on loose

--- Player.h
+++ Player.h
@@ -8,8 +8,10 @@ class Player : public Entity{
     protected:
         float ExperienceCurrent;
         float ExperienceAtLastRound;
+        float ExperienceStartOfFight;
 
         void addExperience(float);
+        void looseGainedLevels();
 
     public:
         Player(const int,const int,const std::string);
