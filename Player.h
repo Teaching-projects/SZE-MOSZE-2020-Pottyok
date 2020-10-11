665e915330e6a21214e75195d69b9c8d80f2953c
tamasori <oeri.tamas@gmail.com>
2020-10-11 02:28:50 +0200

    remove cc from Player

--- Player.h
+++ Player.h
@@ -12,7 +12,6 @@ class Player : public Entity{
 
     public:
         Player(const int health,const int attackDamage,const std::string& name) : Entity(health, attackDamage, name), ExperienceCurrent(0) {};
-        Player(const Player& other) : Entity(other.Health, other.AttackDamage, other.Name), ExperienceCurrent(other.ExperienceCurrent){};
         Player& operator=(const Entity&);
         int getLevel() const;
         void attack(Entity&);
