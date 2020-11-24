9d8296c5d4ecb109855fa2f31cb43ede30b08bf4
balintxd <43203027+balintxd@users.noreply.github.com>
2020-11-24 09:11:05 +0000

    Update gh-pages to output generated at

--- search/all_6.js
+++ search/all_6.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['health',['Health',['../classMonster.html#a9aabfd58aed1ef42e2a038cb27b1262b',1,'Monster']]],
   ['healthpointbonusperlevel',['HealthPointBonusPerLevel',['../classHero.html#abc239daac29da8a4421a5bc61dda1092',1,'Hero']]],
-  ['hero',['Hero',['../classHero.html',1,'Hero'],['../classHero.html#a23ee3fa9762dcdb5715446224fd4fa9e',1,'Hero::Hero(const float health, const float attackDamage, const std::string &amp;name, const float attackSpeed, float ExperiencePerLevel, float HealthPointBonusPerLevel, float DamageBonusPerLevel, float ColdownMultiplierPerLevel)'],['../classHero.html#a1e7352b9d4070fe3f088cceff7bfe1df',1,'Hero::Hero(Monster monster)']]]
+  ['hero',['Hero',['../classHero.html',1,'Hero'],['../classHero.html#a88c836e04f45afd8130f3b9ecd0dc481',1,'Hero::Hero(const float health, const float attackDamage, const float defense, const std::string &amp;name, const float attackSpeed, float ExperiencePerLevel, float HealthPointBonusPerLevel, float DamageBonusPerLevel, float DefenseBonusPerLevel, float ColdownMultiplierPerLevel)'],['../classHero.html#a1e7352b9d4070fe3f088cceff7bfe1df',1,'Hero::Hero(Monster monster)']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hero',['Hero',['../classHero.html#a23ee3fa9762dcdb5715446224fd4fa9e',1,'Hero::Hero(const float health, const float attackDamage, const std::string &amp;name, const float attackSpeed, float ExperiencePerLevel, float HealthPointBonusPerLevel, float DamageBonusPerLevel, float ColdownMultiplierPerLevel)'],['../classHero.html#a1e7352b9d4070fe3f088cceff7bfe1df',1,'Hero::Hero(Monster monster)']]]
+  ['hero',['Hero',['../classHero.html#a88c836e04f45afd8130f3b9ecd0dc481',1,'Hero::Hero(const float health, const float attackDamage, const float defense, const std::string &amp;name, const float attackSpeed, float ExperiencePerLevel, float HealthPointBonusPerLevel, float DamageBonusPerLevel, float DefenseBonusPerLevel, float ColdownMultiplierPerLevel)'],['../classHero.html#a1e7352b9d4070fe3f088cceff7bfe1df',1,'Hero::Hero(Monster monster)']]]
 ];
