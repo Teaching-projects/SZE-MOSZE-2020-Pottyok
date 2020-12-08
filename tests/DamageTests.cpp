268e7d2395f03ff93eb21271d78c43023031373e
tamasori <oeri.tamas@gmail.com>
2020-12-08 02:19:59 +0100

    Fix DamageTests

--- tests/DamageTests.cpp
+++ tests/DamageTests.cpp
@@ -5,42 +5,42 @@
 TEST(DamageTests, SumDamageWithoutDefense)
 {
     Damage damage = Damage();
-    damage->physical = 4;
-    damage->magical = 4;
+    damage.physical = 4;
+    damage.magical = 4;
 
-    ASSERT_EQ(damage->sum(), 8);
+    ASSERT_EQ(damage.sum(), 8);
 }
 
 TEST(DamageTests, SumDamageWithDefense)
 {
     Damage damage = Damage();
-    damage->physical = 4;
-    damage->magical = 4;
+    damage.physical = 4;
+    damage.magical = 4;
 
-    ASSERT_EQ(damage->sum(4), 4);
+    ASSERT_EQ(damage.sum(4), 4);
 }
 
 TEST(DamageTests, SumDamageWithProgress)
 {
     Damage damage = Damage();
-    damage->physical = 4;
-    damage->magical = 4;
-    damage->progress(5,5);
+    damage.physical = 4;
+    damage.magical = 4;
+    damage.progress(5,5);
 
-    ASSERT_EQ(damage->sum(0), 18);
+    ASSERT_EQ(damage.sum(0), 18);
 }
 
 TEST(DamageTests, AddDamages)
 {
     Damage damage1 = Damage();
-    damage->physical = 4;
-    damage->magical = 4;
+    damage.physical = 4;
+    damage.magical = 4;
     Damage damage2 = Damage();
-    damage->physical = 4;
-    damage->magical = 4;
+    damage.physical = 4;
+    damage.magical = 4;
     damage1 += damage2;
 
-    ASSERT_EQ(damage->sum(0), 16);
+    ASSERT_EQ(damage.sum(0), 16);
 }
 
 int main(int argc, char **argv)
