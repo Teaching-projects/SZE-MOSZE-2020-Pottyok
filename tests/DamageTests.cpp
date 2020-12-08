bdbe8b58fde63e994a197f41e7f91dd1b309efd2
tamasori <oeri.tamas@gmail.com>
2020-12-08 02:12:54 +0100

    missing cmake commands

---
+++ tests/DamageTests.cpp
@@ -0,0 +1,50 @@
+#include "Damage.h"
+
+#include <gtest/gtest.h>
+
+TEST(DamageTests, SumDamageWithoutDefense)
+{
+    Damage damage();
+    damage->physical = 4;
+    damage->magical = 4;
+
+    ASSERT_EQ(damage->sum(), 8);
+}
+
+TEST(DamageTests, SumDamageWithDefense)
+{
+    Damage damage();
+    damage->physical = 4;
+    damage->magical = 4;
+
+    ASSERT_EQ(damage->sum(4), 4);
+}
+
+TEST(DamageTests, SumDamageWithProgress)
+{
+    Damage damage();
+    damage->physical = 4;
+    damage->magical = 4;
+    damage->progress(5,5);
+
+    ASSERT_EQ(damage->sum(0), 18);
+}
+
+TEST(DamageTests, AddDamages)
+{
+    Damage damage1();
+    damage->physical = 4;
+    damage->magical = 4;
+    Damage damage2();
+    damage->physical = 4;
+    damage->magical = 4;
+    damage1 += damage2;
+
+    ASSERT_EQ(damage->sum(0), 16);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
