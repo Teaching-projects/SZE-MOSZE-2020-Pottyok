f5c365742f6c490416f53efcd5dc878b11916bfc
mushukcha <penzespeter4@gmail.com>
2020-12-09 16:25:11 +0100

    remove preparedgame tests form renderer

--- tests/RendererTests.cpp
+++ tests/RendererTests.cpp
@@ -27,27 +27,6 @@ TEST(RendererTests, HeroRenderTest)
     ASSERT_EQ(output.str(), expected);
 }
 
-TEST(RendererTests, ObserverTestPreparedGame)
-{
-    PreparedGame g("preparedgame_test");
-    std::stringstream output;
-    std::string expected("\n╔════════════════════════════╗\n║████████████████████████████║\n║██░░░░░░██░░░░████████░░░░██║\n║██░░████████░░░░████░░░░████║\n║██┣┫M░░░██░░░░████░░░░██████║\n║██████░░██░░████░░░░████████║\n║██░░░░░░░░░░░░░░░░░░████████║\n║████████████████████████████║\n╚════════════════════════════╝\n");
-    g.registerRenderer(new ObserverTextRenderer(output));
-    g.render();
-    ASSERT_EQ(output.str(), expected);
-}
-
-
-TEST(RendererTests, HeroRenderTestPreparedGame)
-{
-    PreparedGame g("preparedgame_test");
-    std::stringstream output;
-    std::string expected("\n╔════════╗\n║██░░░░░░║\n║██░░████║\n║██┣┫M░░░║\n║██████░░║\n║██░░░░░░║\n╚════════╝\n");
-    g.registerRenderer(new HeroTextRenderer(output));
-    g.render();
-    ASSERT_EQ(output.str(), expected);
-}
-
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
