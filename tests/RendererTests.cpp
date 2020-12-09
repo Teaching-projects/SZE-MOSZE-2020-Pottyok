57db3459c90d9482d72da2977a6543b645e5dd1c
mushukcha <penzespeter4@gmail.com>
2020-12-09 15:20:19 +0100

    add unittests to renderertests

--- tests/RendererTests.cpp
+++ tests/RendererTests.cpp
@@ -17,12 +17,32 @@ TEST(RendererTests, HeroRenderTest)
 {
     Game g("map_test.txt");
     std::stringstream output;
-    std::string expected("\n╔══════════╗\n║██████████║\n║██░░┣┫░░██║\n║██░░██████║\n║██░░M░M░██║\n╚══════════╝\n");
+    std::string expected("\n╔══════════╗\n║██████████║\n║██░░░░░░██║\n║██░░██████║\n║██░░░░░░██║\n╚══════════╝\n");
     g.registerRenderer(new HeroTextRenderer(output));
     g.render();
     ASSERT_EQ(output.str(), expected);
 }
 
+TEST(RendererTests, ObserverTestPreparedGame)
+{
+    PreparedGame g("preparedgame_test");
+    std::stringstream output;
+    std::string expected("\n╔════════════════════════════╗\n║████████████████████████████║\n║██░░░░░░██░░░░████████░░░░██║\n║██░░████████░░░░████░░░░████║\n║██┣┫M░░░██░░░░████░░░░██████║\n║██████░░██░░████░░░░████████║\n║██░░░░░░░░░░░░░░░░░░████████║\n║████████████████████████████║\n╚════════════════════════════╝\n");
+    g.registerRenderer(new ObserverTextRenderer(output));
+    g.render();
+    ASSERT_EQ(output.str(), expected);
+}
+
+
+TEST(RendererTests, HeroRenderTestPreparedGame)
+{
+    PreparedGame g("preparedgame_test");
+    std::stringstream output;
+    std::string expected("\n╔════════╗\n║██░░░░░░║\n║██░░████║\n║██┣┫M░░░║\n║██████░░║\n║██░░░░░░║\n╚════════╝\n");
+    g.registerRenderer(new HeroTextRenderer(output));
+    g.render();
+    ASSERT_EQ(output.str(), expected);
+}
 
 int main(int argc, char **argv)
 {
